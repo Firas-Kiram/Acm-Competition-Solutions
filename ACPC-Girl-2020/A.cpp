c8c447d259dece5a1f383b07f5de1b9845bedbba
Firas Mohamed El Amine Kiram <135715029+Firas-Kiram@users.noreply.github.com>
2025-07-12 11:20:36 -0700

    Create A.cpp

---
+++ ACPC-Girl-2020/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(c) begin(c), end(c)
+#define pb push_back
+const int N = 5e5 + 100 , LOG = 23;
+
+int main(){
+    freopen("welcome.in", "r", stdin);
+    // freopen("text.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n; cin >> n;
+        cout << n - 2020 << '\n';
+    }
+    
+    return 0;
+}
